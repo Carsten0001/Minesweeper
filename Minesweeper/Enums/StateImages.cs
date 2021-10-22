90b85309ef321126c7c0caa0a8bea537d1fb9923
Carsten <medinfcu@gmail.com>
2021-10-22 20:39:58 +0200

    #4_LLocalized Enums

---
+++ Minesweeper/Enums/StateImages.cs
@@ -0,0 +1,78 @@
+﻿namespace Minesweeper.Enums
+{
+    /// <summary>
+    /// The Names of the Images which could be displayed on the Tiles
+    /// </summary>
+    public enum StateImages
+    {
+        /// <summary>
+        /// The Tile is revealed and has no Mine
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Initial State of Mine
+        /// </summary>
+        Null,
+
+        /// <summary>
+        /// The Tile is revealed and has one Mine
+        /// </summary>
+        One,
+
+        /// <summary>
+        /// The Tile is revealed and has two Mines
+        /// </summary>
+        Two,
+
+        /// <summary>
+        /// The Tile is revealed and has three Mines
+        /// </summary>
+        Three,
+
+        /// <summary>
+        /// The Tile is revealed and has four Mines
+        /// </summary>
+        Four,
+
+        /// <summary>
+        /// The Tile is revealed and has five Mines
+        /// </summary>
+        Five,
+
+        /// <summary>
+        /// The Tile is revealed and has six Mines
+        /// </summary>
+        Six,
+
+        /// <summary>
+        /// The Tile is revealed and has seven Mines
+        /// </summary>
+        Seven,
+
+        /// <summary>
+        /// The Tile is revealed and has eight Mines
+        /// </summary>
+        Eight,
+
+        /// <summary>
+        /// Tile right clicked once
+        /// </summary>
+        Flag,
+
+        /// <summary>
+        /// Tile right clicked twice
+        /// </summary>
+        Questionmark,
+
+        /// <summary>
+        /// The player has lost the game and Mine has been reveled automatically
+        /// </summary>
+        Mine,
+
+        /// <summary>
+        /// The player has clicked a Tile which has bomb
+        /// </summary>
+        Explosion
+    }
+}
--- Minesweeper/Model/MinesCore.cs
+++ Minesweeper/Model/MinesCore.cs
@@ -1,4 +1,5 @@
-﻿using Minesweeper.Properties;
+﻿using Minesweeper.Enums;
+using Minesweeper.Properties;
 using Minesweeper.ViewModels;
 using System;
 using System.Collections;
@@ -650,119 +651,6 @@ namespace Minesweeper.Model
         }
 
         #endregion Methods
-    }
-
-    /// <summary>
-    /// Difficulties which can be choosen from
-    /// </summary>
-    public enum Difficulty
-    {
-        /// <summary>
-        /// 8 X 8; 10 Mines
-        /// </summary>
-        Easy,
-
-        /// <summary>
-        /// 16 X 16; 40 Mines
-        /// </summary>
-        Normal,
-
-        /// <summary>
-        /// 16 X 30; 99 Mines
-        /// </summary>
-        Hard
-    }
-
-    /// <summary>
-    /// The two GameModes
-    /// </summary>
-    public enum GameMode
-    {
-        /// <summary>
-        /// Game will be built dependend on diffiulty choosen.
-        /// </summary>
-        Standard,
-
-        /// <summary>
-        /// Size and Number of Mines can be customized. Difficulty has no effect.
-        /// </summary>
-        Custom
-    }
-
-    /// <summary>
-    /// The Names of the Images which could be displayed on the Tiles
-    /// </summary>
-    public enum StateImages
-    {
-        /// <summary>
-        /// The Tile is revealed and has no Mine
-        /// </summary>
-        None,
-
-        /// <summary>
-        /// Initial State of Mine
-        /// </summary>
-        Null,
-
-        /// <summary>
-        /// The Tile is revealed and has one Mine
-        /// </summary>
-        One,
-
-        /// <summary>
-        /// The Tile is revealed and has two Mines
-        /// </summary>
-        Two,
-
-        /// <summary>
-        /// The Tile is revealed and has three Mines
-        /// </summary>
-        Three,
-
-        /// <summary>
-        /// The Tile is revealed and has four Mines
-        /// </summary>
-        Four,
-
-        /// <summary>
-        /// The Tile is revealed and has five Mines
-        /// </summary>
-        Five,
-
-        /// <summary>
-        /// The Tile is revealed and has six Mines
-        /// </summary>
-        Six,
-
-        /// <summary>
-        /// The Tile is revealed and has seven Mines
-        /// </summary>
-        Seven,
-
-        /// <summary>
-        /// The Tile is revealed and has eight Mines
-        /// </summary>
-        Eight,
-
-        /// <summary>
-        /// Tile right clicked once
-        /// </summary>
-        Flag,
-
-        /// <summary>
-        /// Tile right clicked twice
-        /// </summary>
-        Questionmark,
-
-        /// <summary>
-        /// The player has lost the game and Mine has been reveled automatically
-        /// </summary>
-        Mine,
-
-        /// <summary>
-        /// The player has clicked a Tile which has bomb
-        /// </summary>
-        Explosion
     }
 
     internal class Unsubscriber<MineData> : IDisposable
