ca4924e49060410a66cded66ba32e8fe504feea7
Carsten0001 <medinfcu@gmail.com>
2022-07-07 16:17:32 +0200

    #8 Implemented SoundManager

---
+++ Minesweeper/Manager/SoundManager.cs
@@ -0,0 +1,43 @@
+﻿using Minesweeper.Properties;
+using System;
+using System.ComponentModel;
+using System.Media;
+
+namespace Minesweeper.Manager
+{
+    internal class SoundManager : IDisposable
+    {
+        private readonly SoundPlayer _soundPlayer;
+
+        public SoundManager()
+        {
+            _soundPlayer = new SoundPlayer();
+
+            _soundPlayer.LoadCompleted += SoundPlayer_LoadCompleted;
+
+            _soundPlayer.StreamChanged += SoundPlayer_StreamChanged;
+        }
+
+        private void SoundPlayer_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error == null)
+                _soundPlayer.Play();
+            _soundPlayer.Stream.Dispose();
+        }
+
+        private void SoundPlayer_StreamChanged(object sender, EventArgs e)
+        {
+            _soundPlayer.LoadAsync();
+        }
+
+        public void PlayExplosion()
+        {
+            _soundPlayer.Stream = Resources.Explosion1;
+        }
+
+        public void Dispose()
+        {
+            _soundPlayer.Dispose();
+        }
+    }
+}
